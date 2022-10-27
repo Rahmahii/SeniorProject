7990c92497746779f6d0157593344801d1adb746
رحمة أبوجياب <abujayyabr@gmail.com>
2022-10-28 01:54:51 +0300

    initial commit

--- controllers/invoice.controller.js
+++ controllers/invoice.controller.js
@@ -16,59 +16,59 @@ function create(req, res) {
         depositCardHolder: req.body.depositCardHolder,
         depositCardNum: req.body.depositCardNum,
         depositBankName: req.body.depositBankName,
-        IsPaid:false
+        IsPaid: false
     }
-    if(invoiceHeader.gatawayId!=1){  
-        invoiceHeader.IsPaid=true
+    if (invoiceHeader.gatawayId != 1) {
+        invoiceHeader.IsPaid = true
     }
     //start new 
     i = []
-   
+
     models.invoice_header.create(invoiceHeader).then(invoice_header => {
         if (invoice_header) {
             models.store.findByPk(invoiceHeader.storeId).then(store => {
 
-            var items = req.body.items
-            console.log(items)
+                var items = req.body.items
+                console.log(items)
 
-            for (let index = 0; index < items.length; index++) {
-                console.log(items[index].productId)
-                models.product.findByPk(items[index].productId).then(product => {
-                    if (product.storeId == invoiceHeader.storeId) {
-                        console.log("from iside condetion")
+                for (let index = 0; index < items.length; index++) {
+                    console.log(items[index].productId)
+                    models.product.findByPk(items[index].productId).then(product => {
+                        if (product.storeId == invoiceHeader.storeId) {
+                            console.log("from iside condetion")
 
-                        var item = {
-                            invoiceHeaderId: invoice_header.id,
-                            productId: items[index].productId,
-                            quantity: items[index].quantity,
-                            PurchasingPrice: items[index].PurchasingPrice,
-                            
-                        }
-                        item.product=product.name
-                        models.invoice_detail.create(item).then(invoice_detail => {
-                            invoice_detail.product=product.name
-                          
-                            i.push(item)
-                            
-                            if (invoice_detail && index == items.length - 1) {
-                                res.status(201).json({
-                                    message: "invoice create successfully ",
-                                    invoice_header,
-                                    store:store.name,
-                                    items: i,
-                                    status: true,
-                                })
+                            var item = {
+                                invoiceHeaderId: invoice_header.id,
+                                productId: items[index].productId,
+                                quantity: items[index].quantity,
+                                PurchasingPrice: items[index].PurchasingPrice,
+
                             }
-                        })
-                    } else {
-                        res.status(200).json({
-                            message: "item cannot belong to this store ",
-                            status: false
-                        })
-                    }
-                })
-            }
-        })
+                            item.product = product.name
+                            models.invoice_detail.create(item).then(invoice_detail => {
+                                invoice_detail.product = product.name
+
+                                i.push(item)
+
+                                if (invoice_detail && index == items.length - 1) {
+                                    res.status(201).json({
+                                        message: "invoice create successfully ",
+                                        invoice_header,
+                                        store: store.name,
+                                        items: i,
+                                        status: true,
+                                    })
+                                }
+                            })
+                        } else {
+                            res.status(200).json({
+                                message: "item cannot belong to this store ",
+                                status: false
+                            })
+                        }
+                    })
+                }
+            })
         }
     }).catch(error => {
         res.status(400).json({
@@ -78,14 +78,14 @@ function create(req, res) {
         })
     })
 }
-
-function getUserInvoices(req, res) {
+function getUserStoreInvoices(req, res) {
     const user = req.body.userId
-    models.user.findByPk(user).then(result => {
-        if (result) {
+    const store = req.body.storeId
+    models.user.findByPk(user).then(result1 => {
+        if (result1) {
             models.invoice_header.findAll({
-                where: { userId: user },
-                attributes: ['totalPrice','createdAt'],
+                where: { userId: user, storeId: store },
+                attributes: ['totalPrice', 'createdAt'],
                 include: [{
                     model: models.invoice_detail,
                     attributes: ['quantity'],
@@ -93,7 +93,50 @@ function getUserInvoices(req, res) {
                         model: models.product,
                         attributes: ['name', 'sellPrice'],
                     },
-                   ]
+                    ]
+                },
+                {
+                    model: models.payment_gatway,
+                    attributes: ['name']
+                },
+                ],
+            }).then(result => {
+                res.status(201).json({ user: result1.name, invoices: result })
+            }).catch(error => {
+                res.status(500).json({
+                    message: "something went wrong ",
+                    error: error,
+                    status: false
+                })
+            })
+        }
+
+    }).catch(error => {
+        res.status(400).json({
+            message: "something went wrong",
+            error: error,
+            status: false
+        })
+    })
+
+
+}
+
+function getUserInvoices(req, res) {
+    const user = req.body.userId
+    models.user.findByPk(user).then(result => {
+        if (result) {
+            models.invoice_header.findAll({
+                where: { userId: user },
+                attributes: ['totalPrice', 'createdAt'],
+                include: [{
+                    model: models.invoice_detail,
+                    attributes: ['quantity'],
+                    include: [{
+                        model: models.product,
+                        attributes: ['name', 'sellPrice'],
+                    },
+                    ]
                 },
                 {
                     model: models.payment_gatway,
@@ -138,7 +181,7 @@ function getStoreInvoices(req, res) {
         // 
         include: [{
             model: models.user,
-            attributes: ['name', 'phone'],
+            attributes: ['id','name', 'phone'],
         },
         {
             model: models.store,
@@ -177,39 +220,40 @@ function dashboard(req, res) {
     })
 }
 async function dashboard_2(req, res) {
-    
+
     // models.product.hasMany(models.invoice_detail)
     // models.invoice_detail.belongsTo(models.product)
-        const storeId = req.body.storeId
-        models.product.findAll({
-            where: { storeId },
-            attributes: ['name'],
-            include: [{
-                
-                model: models.invoice_detail,
-                attributes: [[Sequelize.fn('count', Sequelize.col('productId')), 'total'],
-                [Sequelize.fn('sum', Sequelize.col('quantity')), 'quantity']], 
-                order: [['invoice_detail.total', 'DESC']],      
-            }],
-            limit: 1,
-            group: ['name'],
-            
-        }).then(result => {
-            res.status(201).json(result)
-        }).catch(error => {
-            res.status(500).json({
-                message: "something went wrong ",
-                error: error,
-                status: false
-            })
-        })
+    const storeId = req.body.storeId
+    models.product.findAll({
+        where: { storeId },
+        attributes: ['name'],
+        include: [{
 
-  
+            model: models.invoice_detail,
+            attributes: [[Sequelize.fn('count', Sequelize.col('productId')), 'total'],
+            [Sequelize.fn('sum', Sequelize.col('quantity')), 'quantity']],
+            order: [['invoice_detail.total', 'DESC']],
+        }],
+        limit: 1,
+        group: ['name'],
+
+    }).then(result => {
+        res.status(201).json(result)
+    }).catch(error => {
+        res.status(500).json({
+            message: "something went wrong ",
+            error: error,
+            status: false
+        })
+    })
+
+
 }
 module.exports = {
     create,
     getUserInvoices,
     getStoreInvoices,
+    getUserStoreInvoices,
     dashboard,
     dashboard_2
 }
--- routes/invoice.route.js
+++ routes/invoice.route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post("/AddInvoice", invoiceController.create)
 router.post("/GetInvoices", invoiceController.getUserInvoices)
+router.post("/getUserStoreInvoices", middleware.checkAuth ,invoiceController.getUserStoreInvoices)
 router.post("/getStoreInvoices",middleware.checkAuth ,invoiceController.getStoreInvoices)
 router.post("/dashboard", middleware.checkAuth,invoiceController.dashboard)
 router.post("/dashboard2", middleware.checkAuth, invoiceController.dashboard_2)
